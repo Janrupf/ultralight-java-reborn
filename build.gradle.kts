1a60c37d91677d39fe18b96dd801e693b66c056a
Janrupf <business.janrupf@gmail.com>
2023-07-19 15:42:51 +0200

    Configure project with sources, javadocs and proper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,19 @@ subprojects {
         extensions.getByType(JavaPluginExtension::class.java).apply {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
+
+            withJavadocJar()
+            withSourcesJar()
+        }
+
+        tasks.withType(JavaCompile::class.java) {
+            options.encoding = "UTF-8"
+        }
+
+        tasks.named<Javadoc>("javadoc") {
+            (options as StandardJavadocDocletOptions).addBooleanOption("Xdoclint:-missing", true)
         }
     }
+
+    version = rootProject.version
 }
